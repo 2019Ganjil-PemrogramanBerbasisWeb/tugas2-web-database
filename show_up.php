c8a32f10b21bf4fe885431f20d3b646bdac20326
lumbricina <55181574+lumbricina@users.noreply.github.com>
2019-11-20 15:28:37 +0700

    update upload

---
+++ show_up.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "kospin";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id_file, name_files, link_files FROM upload";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>ID</th><th>Name</th><th>Link</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["id_file"]. "</td><td>" . $row["name_files"]. "</td><td>" . $row["link_files"]. "</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
+<a href="upload.php">Upload</a>
+<a href="index.php">Balik Home</a>
+</body>
+</html>
