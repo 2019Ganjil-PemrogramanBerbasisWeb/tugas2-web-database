4f92f91fb45e9185342360445c73717d2821aa35
hisyamzf <55182043+hisyamzf@users.noreply.github.com>
2019-11-20 15:27:18 +0700

    uploada
    a

---
+++ upload.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+	<form action="uploadaksi.php" method="post" enctype="multipart/form-data">
+		<input type="file" name="fileToUpload">
+		<input type="submit" name="upload" value="Upload">
+	</form>
+    <a href="show_up.php">Lihat file yang sudah terupload</a>
+</body>
+</html>
